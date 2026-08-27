5379923789767bd7b6979cd4125bc180122e4a1b
Fernanda-Melendrez <mmelendrezj001@uaemex.mx>
2026-08-26 20:11:33 -0600

    PRACTICA_5

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica5;
+
+/**
+ *
+ * @author Fernanda
+ */
+public class Main
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       Practica5 practica5 = new Practica5();
+       
+    }
+    
+}
